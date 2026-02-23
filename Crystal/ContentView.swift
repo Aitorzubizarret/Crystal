409d2925d6421fafc84e6e3a4541a33b3f50c636
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2026-02-23 13:36:47 +0100

    Minor changes

--- Crystal/ContentView.swift
+++ Crystal/ContentView.swift
@@ -14,32 +14,49 @@ struct ContentView: View {
     @EnvironmentObject private var router: AppRouter
     @EnvironmentObject private var photoPrism: PhotoPrismAPIClient
     
+    // MARK: - Methods
+    
+    @ViewBuilder
+    var startView: some View {
+        let startViewModel = StartViewModel(router: router,
+                                            photoPrism: photoPrism)
+        StartView(viewModel: startViewModel)
+    }
+    
+    @ViewBuilder
+    var loginView: some View {
+        let loginViewModel = LoginViewModel(router: router,
+                                            photoPrism: photoPrism)
+        LoginView(viewModel: loginViewModel)
+        .onChange(of: photoPrism.activeSession) { oldValue, newValue in
+            if let _ = newValue {
+                router.navigate(to: .main) // After the user logs in correctly, navigate to the main view.
+            }
+        }
+    }
+    
+    @ViewBuilder
+    var mainView: some View {
+        let mainViewModel = MainViewModel(router: router,
+                                          photoPrism: photoPrism)
+        MainView(viewModel: mainViewModel)
+        .onChange(of: photoPrism.activeSession) { oldValue, newValue in
+            if newValue == nil {
+                router.navigate(to: .login) // After deleting the user's session, navigate to the login view.
+            }
+        }
+    }
+    
     // MARK: - View
     
     var body: some View {
         switch router.currentRoute {
         case .start:
-            let startViewModel = StartViewModel(router: router,
-                                                photoPrism: photoPrism)
-            StartView(viewModel: startViewModel)
+            startView
         case .login:
-            let loginViewModel = LoginViewModel(router: router,
-                                                photoPrism: photoPrism)
-            LoginView(viewModel: loginViewModel)
-                .onChange(of: photoPrism.activeSession) { oldValue, newValue in
-                    if let _ = newValue {
-                        router.navigate(to: .main)
-                    }
-                }
+            loginView
         case .main:
-            let mainViewModel = MainViewModel(router: router,
-                                              photoPrism: photoPrism)
-            MainView(viewModel: mainViewModel)
-                .onChange(of: photoPrism.activeSession) { oldValue, newValue in
-                    if newValue == nil {
-                        router.navigate(to: .login)
-                    }
-                }
+            mainView
         }
     }
 }
